54ac3e0bd326ae792513e203e8d6e306bb775177
bharath1214228 <bharathmadamanchi5@gmail.com>
2022-10-28 22:52:09 +0530

    upload

---
+++ gracethemes.com/demo/medplus/wp-content/themes/medplus-pro/js/custom-animation45f6.js
@@ -0,0 +1,99 @@
+jQuery(window).scroll(function() {							   
+	jQuery('.appointmentbx').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+		});
+	
+	jQuery('.pgboxwrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+		});
+	
+	jQuery('.buynowwrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInDown");
+			}
+		});	
+		 
+	jQuery('.abtthemewrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+		});	
+		
+	jQuery('.teamareawrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+		});	
+		
+	 jQuery('.featureswrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+		});	
+	 
+	 jQuery('.skillswrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+		});		
+	 
+	 jQuery('.newsareawrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInDown");
+			}
+		});	
+	 
+	 jQuery('.tmareawrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInUp");
+			}
+		});	
+	 
+	 jQuery('.dpareawrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+		});	
+	 
+	  jQuery('.gallerywrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+		});
+	  
+	  jQuery('.clientwrap').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInUp");
+			}
+		});	
+		
+		
+	});
